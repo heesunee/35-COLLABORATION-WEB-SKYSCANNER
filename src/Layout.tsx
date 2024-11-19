a4c71a68eb6aca63fd64eba7bb2076aa1d139062
minjeoong <orange_mj@naver.com>
2024-11-19 21:38:43 +0900

    feat : path 파일 생성 및 폴더구조 변경

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header.tsx';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
 	return (
