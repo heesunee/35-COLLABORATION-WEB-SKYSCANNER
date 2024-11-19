40c2f20db8117cf1e5791b59306d973efb64ae11
minjeoong <orange_mj@naver.com>
2024-11-19 21:18:42 +0900

    feat : 필터 버튼 css 수정

--- src/components/common/FilterButton.tsx
+++ src/components/common/FilterButton.tsx
@@ -24,26 +24,36 @@ const FilterButton = ({ content, Icon }: props) => {
 	);
 };
 
+export const ButtonStyles = {
+	default: css`
+		display: flex;
+		width: 6.4rem;
+		height: 2.8rem;
+		padding: 0.7rem 1.2rem;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+
+		border-radius: 1.45rem;
+		border: 0.1rem solid ${({ theme }) => theme.colors.grey30};
+		background: ${({ theme }) => theme.colors.darksky};
+
+		color: ${({ theme }) => theme.colors.white};
+		text-align: center;
+		font-family: 'Apple SD Gothic Neo';
+		font-size: 12px;
+		font-weight: 800;
+	`,
+	Clicked: css`
+		background: ${({ theme }) => theme.colors.skyblue};
+		border: none;
+	`,
+};
+
 const FilterButtonContainer = styled.div<StyledProps>`
-	display: flex;
-	width: 6.4rem;
-	height: 2.8rem;
-	padding: 0.7rem 1.2rem;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 1rem;
-
-	border-radius: 1.45rem;
-	border: 0.1rem solid ${({ theme }) => theme.colors.grey30};
-	background: ${({ theme }) => theme.colors.darksky};
-
-	${({ isClicked, theme }) =>
-		isClicked &&
-		css`
-			border-radius: 14.5px;
-			background: ${theme.colors.skyblue};
-		`}
+	${ButtonStyles.default}
+	${(props) => props.isClicked && ButtonStyles['Clicked']}
 `;
 
 export default FilterButton;
