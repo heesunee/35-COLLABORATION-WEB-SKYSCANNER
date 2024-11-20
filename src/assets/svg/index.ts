9b99a897900b1004b969a22967263884d93133de
minjeoong <orange_mj@naver.com>
2024-11-20 18:35:48 +0900

    fix : svg index 에 절대경로 설정

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,10 +1,10 @@
-import IcCarWhite from './ic_car_white.svg?react';
-import IcFlagKorea from './ic_flag_korea.svg?react';
-import IcFlightWhite from './ic_flight_white.svg?react';
-import IcHotelWhite from './ic_hotel_white.svg?react';
-import IcLikePage from './ic_likepage.svg?react';
-import IcMainLogo from './ic_mainolgo_white.svg?react';
-import IcMy from './ic_my.svg?react';
-import IcSkyscannerLogo from './ic_skyscanner_logo.svg?react';
+import IcCarWhite from '@/assets/svg/ic_car_white.svg?react';
+import IcFlagKorea from '@/assets/svg/ic_flag_korea.svg?react';
+import IcFlightWhite from '@/assets/svg/ic_flight_white.svg?react';
+import IcHotelWhite from '@/assets/svg/ic_hotel_white.svg?react';
+import IcLikePage from '@/assets/svg/ic_likepage.svg?react';
+import IcMainLogo from '@/assets/svg/ic_mainolgo_white.svg?react';
+import IcMy from '@/assets/svg/ic_my.svg?react';
+import IcSkyscannerLogo from '@/assets/svg/ic_skyscanner_logo.svg?react';
 
 export { IcSkyscannerLogo, IcMainLogo, IcLikePage, IcMy, IcFlagKorea, IcCarWhite, IcHotelWhite, IcFlightWhite };
