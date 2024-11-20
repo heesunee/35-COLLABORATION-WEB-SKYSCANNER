486fa3ead0048182ebf2ed770cdc7bfb6a03e8ce
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-11-21 03:08:38 +0900

    feat: Tab 합성 컴포넌트 구현

--- src/pages/wish/index.tsx
+++ src/pages/wish/index.tsx
@@ -1,6 +1,22 @@
+import Tab from '@/components/wish/Tab';
 
-const Wish = () => {
-  return <>Wish</>;
-}
+const App = () => {
+	return (
+		<Tab.Root defaultActiveTab={0}>
+			<Tab.List>
+				<Tab.Item index={0}>전체</Tab.Item>
+				<Tab.Item index={1}>항공권</Tab.Item>
+				<Tab.Item index={2}>호텔</Tab.Item>
+				<Tab.Item index={3}>렌터카</Tab.Item>
+			</Tab.List>
+			<Tab.Panels>
+				<Tab.Panel>전체 내용</Tab.Panel>
+				<Tab.Panel>항공권 내용</Tab.Panel>
+				<Tab.Panel>호텔 내용</Tab.Panel>
+				<Tab.Panel>렌터카 내용</Tab.Panel>
+			</Tab.Panels>
+		</Tab.Root>
+	);
+};
 
-export default Wish;
+export default App;
