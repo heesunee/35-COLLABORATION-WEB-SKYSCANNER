a48531d7b10d3c7bbdf0c2947709f688ef907a99
minjeoong <orange_mj@naver.com>
2024-11-21 22:01:30 +0900

    feat : inputForm 컴포넌트 구현

---
+++ src/components/common/InputForm.tsx
@@ -0,0 +1,42 @@
+import { flexCssGenerator } from '@/styles/customStyle.ts';
+import React from 'react';
+import styled from 'styled-components';
+
+interface InputFormProps {
+	Icon?: React.ComponentType;
+	placeholder: string;
+}
+
+const InputForm = ({ Icon, placeholder }: InputFormProps) => {
+	return (
+		<InputFormContainer>
+			{Icon && <Icon />}
+			<StyledInput type="text" placeholder={placeholder} />
+		</InputFormContainer>
+	);
+};
+
+const InputFormContainer = styled.div`
+	${flexCssGenerator('flex', 'center', 'flex-start')};
+	gap: 0.1rem;
+	border-radius: 1.1rem;
+	border: 0.1rem solid ${({ theme }) => theme.colors.grey30};
+	background: ${({ theme }) => theme.colors.white};
+	margin-top: 1.9rem;
+	min-height: 3.9rem;
+	padding: 0.8rem 1rem;
+`;
+
+const StyledInput = styled.input`
+	flex: 1;
+	border: none;
+	outline: none;
+	color: ${({ theme }) => theme.colors.black};
+	${({ theme }) => theme.fonts.body2_sb_14}
+
+	&::placeholder {
+		color: ${({ theme }) => theme.colors.grey30};
+	}
+`;
+
+export default InputForm;
