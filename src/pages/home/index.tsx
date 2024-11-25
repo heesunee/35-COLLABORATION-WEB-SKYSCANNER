fd8745966dfe7204f4ea4cec723097907a64179b
GaYeonKim <ayla_12@naver.com>
2024-11-25 18:00:49 +0900

    fix: 테스트 코드 삭제

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,9 @@
-import RecommendCard from '@/components/home/RecommendCard';
 import CalenderModal from '@/components/home/calender-modal.tsx';
-import { mockRecommendList } from '@/mocks/mockRecommendList';
 
 const Home = () => {
 	return (
 		<>
 			<CalenderModal />
-			<>
-				{mockRecommendList.map((item) => (
-					<RecommendCard key={item.id} {...item} />
-				))}
-			</>
 		</>
 	);
 };
