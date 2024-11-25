1669162b73aa69dc54f4ff569351cf632846252e
minjeoong <orange_mj@naver.com>
2024-11-25 18:13:34 +0900

    fix : 콘솔 로그 제거

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -14,7 +14,6 @@ const Home = () => {
 	});
 
 	const onModalToggle = (modal: 'search' | 'calender') => {
-		console.log(isModalOpen);
 		setModalOpen({
 			...isModalOpen,
 			[modal]: !isModalOpen[modal],
