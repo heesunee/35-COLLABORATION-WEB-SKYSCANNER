d7152b6dde08b0e7d7f4e6f923ad2deb968a53b3
Heesun Park <phs1585@naver.com>
2024-11-29 13:32:46 +0900

    feat: 사용하지 않는 타입 삭제

--- src/types/FlightTypes.ts
+++ src/types/FlightTypes.ts
@@ -1,19 +1,3 @@
-export interface Flight {
-	id: number;
-	from: string;
-	to: string;
-	dep_time1: string;
-	arr_time1: string;
-	dep_time2: string;
-	arr_time2: string;
-	average_time: string;
-	dep_price: number;
-	arr_price: number;
-	airline: string;
-	is_like: boolean;
-	count: number;
-}
-
 // flight 객체의 타입을 정의
 export interface Flights {
 	id: number;
