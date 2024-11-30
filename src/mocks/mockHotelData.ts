c2ceba7c07d6b6867920f695e5e258ff0002b3e4
heesunee <phs1585@naver.com>
2024-11-30 01:32:00 +0000

    refactor: 목데이터 이미지 경로 수정

--- src/mocks/mockHotelData.ts
+++ src/mocks/mockHotelData.ts
@@ -1,7 +1,7 @@
 export const Hotels = [
 	{
 		id: 1,
-		image: '/src/assets/img/img_grandhotel.png',
+		image: 'https://github.com/user-attachments/assets/c46fe558-f19c-4a30-86b1-7f66502a5709',
 		name: '오키나와 그랜드 메르 리조트',
 		description: '도심에서 8.59 km',
 		star: 3,
@@ -11,7 +11,7 @@ export const Hotels = [
 	},
 	{
 		id: 2,
-		image: '/src/assets/img/img_montorehotel.png',
+		image: 'https://github.com/user-attachments/assets/ed600c62-81a9-408b-bb59-0b27e703e82f',
 		name: '호텔 몬토레 오키나와 리조트',
 		description: '도심에서 8.59 km',
 		star: 5,
@@ -21,7 +21,7 @@ export const Hotels = [
 	},
 	{
 		id: 3,
-		image: '/src/assets/img/img_crystal.png',
+		image: 'https://github.com/user-attachments/assets/d219d979-9f15-4747-8c45-89ed199defa4',
 		name: '리콘 크리스탈 호텔',
 		description: '도심에서 7.12 km',
 		star: 5,
@@ -34,7 +34,7 @@ export const Hotels = [
 export const hotelData = [
 	{
 		id: 1,
-		image: '/src/assets/img/hotel1.png',
+		image: 'https://github.com/user-attachments/assets/98fe331f-c9c7-4e25-8858-e20f64a6d574',
 		name: '넥서스 리조트 & 스타 가람부 나이',
 		place: '말레이시아 코타키나바루',
 		firstScore: 4,
@@ -47,7 +47,7 @@ export const hotelData = [
 	},
 	{
 		id: 2,
-		image: '/src/assets/img/hotel2.png',
+		image: 'https://github.com/user-attachments/assets/42ad9f88-16cc-4f19-a927-a9d80f6368cb',
 		name: '호텔몬트레이오키나와스파리조트',
 		place: '일본 오키나와',
 		firstScore: 4,
@@ -60,7 +60,7 @@ export const hotelData = [
 	},
 	{
 		id: 3,
-		image: '/src/assets/img/hotel3.png',
+		image: 'https://github.com/user-attachments/assets/3d571618-f97a-48c9-94fc-9e1a2be3b4d6',
 		name: '리콤 크리스탈 호텔',
 		place: '미국 괌',
 		firstScore: 4,
@@ -73,7 +73,7 @@ export const hotelData = [
 	},
 	{
 		id: 4,
-		image: '/src/assets/img/hotel4.png',
+		image: 'https://github.com/user-attachments/assets/de4e8321-97e6-4ced-9f43-b84b8c71a0a4',
 		name: '그랜드 메르 리조트',
 		place: '미국 하와이',
 		firstScore: 4,
@@ -86,7 +86,7 @@ export const hotelData = [
 	},
 	{
 		id: 5,
-		image: '/src/assets/img/hotel5.png',
+		image: 'https://github.com/user-attachments/assets/1226e1db-cda8-4f81-a962-61d0c581bcd0',
 		name: '소피텔 크라비 포키트라 골프 앤 스파 리조트',
 		place: '태국 크라비',
 		firstScore: 4,
@@ -99,7 +99,7 @@ export const hotelData = [
 	},
 	{
 		id: 6,
-		image: '/src/assets/img/hotel6.png',
+		image: 'https://github.com/user-attachments/assets/1cb983eb-a8a7-438e-b5d8-32015fcbca58',
 		name: '더 그랜드 호텔',
 		place: '타이베이 중샨구',
 		firstScore: 4,
